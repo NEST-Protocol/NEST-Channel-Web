14d10df18f0b2293d4702b9f8ec24f6bb3cc5005
tunogya <tunogya@qq.com>
2021-12-09 11:18:44 +0800

    delete unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./pages/App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
+import {ChakraProvider, theme} from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 )
