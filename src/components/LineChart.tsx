decc5f5f7754b9fff187333767954457b4b998d0
tunogya <tunogya@qq.com>
2021-12-09 18:43:36 +0800

    add line chart

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,37 @@
+import {Line} from '@ant-design/charts';
+import React, { useState, useEffect } from 'react';
+import {Stack} from "@chakra-ui/react";
+
+const LineChart = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const asyncFetch = () => {
+    fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log('fetch data failed', error);
+      });
+  };
+
+  const config = {
+    data,
+    xField: "Date",
+    yField: "scales",
+
+    color: "#EAAA00",
+    point: {
+      shape: "circle",
+      size: 4
+    },
+    smooth: true,
+  }
+
+  return <Line {...config}/>
+}
+
+export default LineChart
--- src/pages/Root/QuotationFrequency.tsx
+++ src/pages/Root/QuotationFrequency.tsx
@@ -1,9 +1,13 @@
 import {Stack, Text} from "@chakra-ui/react";
+import LineChart from "../../components/LineChart";
 
 const QuotationFrequency = () => {
   return (
     <Stack bg={"white"} w={"100%"} h={"460px"} borderRadius={20} p={"22px"}>
       <Text fontWeight={600} fontSize={"17px"}>Quotation Frequency</Text>
+      <Stack p={"22px"} h={"100%"}>
+        <LineChart />
+      </Stack>
     </Stack>
   )
 }
