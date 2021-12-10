6992fadc5062c570b89c9012ad67671f22ab3e14
tunogya <tunogya@qq.com>
2021-12-10 22:24:27 +0800

    fixed

--- src/pages/Create/Done.tsx
+++ src/pages/Create/Done.tsx
@@ -1,9 +1,11 @@
 import {Button, Stack, Text} from "@chakra-ui/react";
 import {useRecoilState} from "recoil";
 import {activeStepAtom} from "../../state/Create/activeStepAtom";
+import {useNavigate} from "react-router-dom";
 
 const Done = () => {
   const [activeStep, setActiveStep] = useRecoilState(activeStepAtom)
+  const navigate = useNavigate()
 
   return (
     <Stack pt={"60px"} pb={"30px"} w={"600px"} h={"400px"} spacing={"20px"} alignItems={"center"}>
@@ -12,6 +14,12 @@ const Done = () => {
         setActiveStep(0)
       }}>
         Recreate
+      </Button>
+      <Button w={"176px"} variant={"outline"} onClick={()=> {
+        navigate("/")
+        setActiveStep(0)
+      }}>
+        Back
       </Button>
     </Stack>
   )
