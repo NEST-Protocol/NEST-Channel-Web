1d150e21dcdfec029962bc4d3a4a2e63593bd761
tunogya <tunogya@qq.com>
2021-12-13 10:40:05 +0800

    fixed color

--- src/pages/Root/Information.tsx
+++ src/pages/Root/Information.tsx
@@ -34,7 +34,7 @@ const InformationDetail: FC<InformationDetailProps> = ({...props}) => {
   return (
     <WrapItem>
       <Stack direction={"row"} w={"300px"}>
-        <Text color={"secondary"} fontWeight={"600"}>{ props.title }</Text>
+        <Text color={"secondary.500"} fontWeight={"600"}>{ props.title }</Text>
         <Spacer/>
         { props.link ? (
           <Link href={props.link} isExternal color={"link.500"} fontWeight={"bold"}>{props.value} {props.unit}</Link>
