b7ad438d957cedcc50327dfe05e39c01b88f5b25
tunogya <tunogya@qq.com>
2021-12-15 17:19:50 +0800

    add Nest contracts

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -3,11 +3,15 @@ import { Contract } from '@ethersproject/contracts'
 import ERC20_ABI from '../abis/erc20.json'
 import ERC20_BYTES32_ABI from '../abis/erc20_bytes32.json'
 import EIP_2612 from '../abis/eip_2612.json'
+import NestOpenPlatform_ABI from '../abis/nestOpenPlatform.json'
+import NestBatchPlatform2_ABI from '../abis/nestBatchPlatform2.json'
+import NestLedger_ABI from '../abis/nestLedger.json'
+import NestGovernance_ABI from '../abis/nestGovernance.json'
 
 import { useActiveWeb3React } from './web3'
 import {getContract} from "../utils";
 import {useMemo} from "react";
-import {Erc20} from "../abis/types";
+import {Erc20, NestOpenPlatform, NestBatchPlatform2, NestGovernance, NestLedger} from "../abis/types";
 
 // returns null on errors
 export function useContract<T extends Contract = Contract>(
@@ -42,5 +46,21 @@ export function useBytes32TokenContract(tokenAddress?: string, withSignerIfPossi
 
 export function useEIP2612Contract(tokenAddress?: string): Contract | null {
   return useContract(tokenAddress, EIP_2612, false)
+}
+
+export function useNestOpenPlatform(address: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract<NestOpenPlatform>(address, NestOpenPlatform_ABI, withSignerIfPossible)
+}
+
+export function useNestBatchPlatform2(address: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract<NestBatchPlatform2>(address, NestBatchPlatform2_ABI, withSignerIfPossible)
+}
+
+export function useNestLedger(address: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract<NestLedger>(address, NestLedger_ABI, withSignerIfPossible)
+}
+
+export function useNestGovernance(address: string, withSignerIfPossible?: boolean): Contract | null {
+  return useContract<NestGovernance>(address, NestGovernance_ABI, withSignerIfPossible)
 }
 
