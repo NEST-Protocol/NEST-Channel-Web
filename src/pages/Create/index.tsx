08cda8ce6fbe1563c003050fcd97ccec630a8664
tunogya <tunogya@qq.com>
2021-12-17 15:22:30 +0800

    update

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,25 +1,28 @@
-import { Button, Stack, Text } from '@chakra-ui/react'
-import TokenAddress, { TokenAddressTip } from './TokenAddress'
-import Configuration, { ConfigurationTip } from './Configuration'
-import Confirm, { ConfirmTip } from './Confirm'
+import {Button, Stack, Text} from '@chakra-ui/react'
+import TokenAddress, {TokenAddressTip} from './TokenAddress'
+import Configuration, {ConfigurationTip} from './Configuration'
+import Confirm, {ConfirmTip} from './Confirm'
 import Done from './Done'
 import Divider from '../../components/Divider'
-import { FC } from 'react'
-import { useRecoilState } from 'recoil'
-import { activeStepAtom } from '../../state/Create/activeStepAtom'
+import {FC} from 'react'
+import {useRecoilState} from 'recoil'
+import {activeStepAtom} from '../../state/Create/activeStepAtom'
 import useCreateChannel from '../../hooks/useCreateChannel'
 import {PROCESSING} from "../../constants/misc";
+import {useTokenContract} from "../../hooks/useContract";
+import {NEST_ADDRESS} from "../../constants/addresses";
+import {useActiveWeb3React} from "../../hooks/web3";
 
 const steps = [
-  { id: 0, label: 'Token Address', content: <TokenAddress /> },
-  { id: 1, label: 'Configuration', content: <Configuration /> },
-  { id: 2, label: 'Confirm', content: <Confirm /> },
+  {id: 0, label: 'Token Address', content: <TokenAddress/>},
+  {id: 1, label: 'Configuration', content: <Configuration/>},
+  {id: 2, label: 'Confirm', content: <Confirm/>},
 ]
 
 const tips = [
-  { id: 0, label: 'Token Address', content: <TokenAddressTip /> },
-  { id: 1, label: 'Configuration', content: <ConfigurationTip /> },
-  { id: 2, label: 'Confirm', content: <ConfirmTip /> },
+  {id: 0, label: 'Token Address', content: <TokenAddressTip/>},
+  {id: 1, label: 'Configuration', content: <ConfigurationTip/>},
+  {id: 2, label: 'Confirm', content: <ConfirmTip/>},
 ]
 
 type StepItemProps = {
@@ -29,9 +32,11 @@ type StepItemProps = {
 
 const OpenChanel = () => {
   const [activeStep, setActiveStep] = useRecoilState(activeStepAtom)
-  const { invalidTokenAddress, invalidConfiguration, create, status } = useCreateChannel()
+  const {invalidTokenAddress, invalidConfiguration, create, status} = useCreateChannel()
+  const { chainId } = useActiveWeb3React()
+  const nestContract = useTokenContract(NEST_ADDRESS[chainId ?? 1], true)
 
-  const StepButton: FC<StepItemProps> = ({ ...props }) => {
+  const StepButton: FC<StepItemProps> = ({...props}) => {
     return (
       <>
         <Button
@@ -62,11 +67,11 @@ const OpenChanel = () => {
           whiteSpace={'nowrap'}
           hidden={activeStep === 3}
         >
-          <StepButton id={0} title={'Token Address'} />
-          <Divider active={activeStep >= 1} />
-          <StepButton id={1} title={'Configuration'} />
-          <Divider active={activeStep >= 2} />
-          <StepButton id={2} title={'Confirm'} />
+          <StepButton id={0} title={'Token Address'}/>
+          <Divider active={activeStep >= 1}/>
+          <StepButton id={1} title={'Configuration'}/>
+          <Divider active={activeStep >= 2}/>
+          <StepButton id={2} title={'Confirm'}/>
         </Stack>
         {steps.map((step) => (
           <Stack hidden={activeStep !== step.id} key={step.id}>
@@ -74,22 +79,36 @@ const OpenChanel = () => {
           </Stack>
         ))}
         {activeStep === 3 ? (
-          <Done />
+          <Done/>
         ) : (
-          <Button
-            w={'176px'}
-            isLoading={status === PROCESSING}
-            disabled={activeStep === steps.length - 1 ? invalidTokenAddress || invalidConfiguration : false}
-            onClick={async () => {
-              if (activeStep === steps.length - 1) {
-                await create()
+          <Stack direction={"row"}>
+            <Button variant={"outline"}
+              onClick={async () => {
+              if (nestContract) {
+                const tx = await nestContract.approve("0x638461F3Ae49CcC257ef49Fe76CCE5816A9234eF", "10000000000000000000000")
+                console.log(tx)
+                const res = await tx.wait()
+                console.log(res)
               }
-              const newStep = activeStep + 1
-              setActiveStep(newStep)
-            }}
-          >
-            {activeStep === steps.length - 1 ? 'Create' : 'Next'}
-          </Button>
+            }}>
+              Approve NEST(test)
+            </Button>
+            <Button
+              w={'176px'}
+              isLoading={status === PROCESSING}
+              disabled={activeStep === steps.length - 1 ? invalidTokenAddress || invalidConfiguration : false}
+              onClick={async () => {
+                if (activeStep === steps.length - 1) {
+                  await create()
+                }
+                const newStep = activeStep + 1
+                setActiveStep(newStep)
+              }}
+            >
+              {activeStep === steps.length - 1 ? 'Create' : 'Next'}
+            </Button>
+          </Stack>
+
         )}
       </Stack>
 
