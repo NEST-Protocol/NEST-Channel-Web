902499bb050c5c4fcd371feb84f520f01ec3adbe
tunogya <tunogya@qq.com>
2021-12-20 15:10:31 +0800

    add deposit

--- src/pages/Root/Administrator.tsx
+++ src/pages/Root/Administrator.tsx
@@ -1,10 +1,25 @@
-import { Button, Input, Popover, PopoverBody, PopoverContent, PopoverTrigger, Stack, Text } from '@chakra-ui/react'
+import {
+  Button,
+  Input,
+  NumberInput, NumberInputField,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Stack,
+  Text
+} from '@chakra-ui/react'
 import {useRecoilValue} from "recoil";
 import {activeChannelIdAtom} from "../../state/Root";
 import useChannelInfo from "../../hooks/useChannelInfo";
 import {useActiveWeb3React} from "../../hooks/web3";
-import {FC} from "react";
-import {PROCESSING} from "../../constants/misc";
+import {FC, useEffect, useState} from "react";
+import {ERROR, IDLE, IDLE_DELAY, PROCESSING, SUCCESS} from "../../constants/misc";
+import {useNestOpenPlatformContract, useTokenContract} from "../../hooks/useContract";
+import {NEST_OPEN_PLATFORM} from "../../constants/addresses";
+import {formatNumber, parseToBigNumber} from "../../utils/bignumberUtil";
+import {useTokenSymbol} from "../../hooks/Tokens";
+import {formatWithUnit, parseToNumber} from "../../utils/unit";
 
 const Administrator = () => {
   const activeChannelId = useRecoilValue(activeChannelIdAtom)
@@ -17,7 +32,7 @@ const Administrator = () => {
         Administrator
       </Text>
       <Stack direction={'row'} spacing={'44px'}>
-        <DepositPopover isLoading={status === PROCESSING} disabled={info?.governance !== account}/>
+        <DepositPopover isLoading={status === PROCESSING} disabled={info?.governance !== account} tokenAddress={info?.reward}/>
         <WithdrawPopover isLoading={status === PROCESSING} disabled={info?.governance !== account}/>
         <WithdrawFeePopover isLoading={status === PROCESSING} disabled={info?.governance !== account}/>
       </Stack>
@@ -28,9 +43,52 @@ const Administrator = () => {
 type PopverProps = {
   disabled: boolean
   isLoading: boolean
+  tokenAddress?: string
 }
 
 const DepositPopover: FC<PopverProps> = ({...props}) => {
+  const { chainId } = useActiveWeb3React()
+  const activeChannelId = useRecoilValue(activeChannelIdAtom)
+  const nestOpenPlatform = useNestOpenPlatformContract(NEST_OPEN_PLATFORM[chainId ?? 1], true)
+  //  function increase(uint channelId, uint96 vault) external payable;
+  const token = useTokenContract(props.tokenAddress)
+  const {account} = useActiveWeb3React()
+  const [balance, setBalance] = useState('0')
+  const [amount, setAmount] = useState('0')
+  const tokenSymbol = useTokenSymbol(props.tokenAddress ?? "")
+  const [status, setStatus] = useState(IDLE)
+
+  const fetch = async () => {
+    if (!token) return
+    const res = await token.balanceOf(account ?? "")
+    setBalance(parseToBigNumber(res).shiftedBy(-18).toFixed(0))
+  }
+
+  const deposit = async () => {
+    if (!nestOpenPlatform) return
+    setStatus(PROCESSING)
+    const tx = await nestOpenPlatform.increase(activeChannelId, parseToBigNumber(amount).shiftedBy(18).toFixed(0))
+    const res = await tx.wait()
+    switch (res.status) {
+      case 0:
+        setStatus(ERROR)
+        setTimeout(()=>{
+          setStatus(IDLE)
+        }, IDLE_DELAY)
+        break
+      case 1:
+        setStatus(SUCCESS)
+        setTimeout(()=>{
+          setStatus(IDLE)
+        }, IDLE_DELAY)
+        break
+    }
+  }
+
+  useEffect(()=>{
+    fetch()
+  }, [chainId, account, props.tokenAddress])
+
   return (
     <Popover>
       <PopoverTrigger>
@@ -40,11 +98,26 @@ const DepositPopover: FC<PopverProps> = ({...props}) => {
         <PopoverBody boxShadow={'0px 0px 60px 0px #BFBFBF'} borderRadius={'20px'}>
           <Stack alignItems={'center'} spacing={'20px'} p={'20px'}>
             <Text fontWeight={'bold'}>Deposit</Text>
-            <Input variant={'filled'} placeholder={'Input Quantity'} />
-            <Text fontWeight={'bold'} fontSize={'sm'} color={'secondary'}>
-              Balance (myself): 80
+            <NumberInput
+              variant={'filled'}
+              onChange={(valueString) => {
+                setAmount(parseToNumber(valueString, tokenSymbol))
+              }}
+              value={formatWithUnit(amount, tokenSymbol)}
+              max={Number(balance)}
+              min={0}
+              onFocus={(e) => {
+                e.target.setSelectionRange(0, amount.length)
+              }}
+            >
+              <NumberInputField />
+            </NumberInput>
+            <Text fontWeight={'bold'} fontSize={'sm'} color={'secondary'} textAlign={"center"}>
+              Balance (myself): {formatNumber(balance)} {tokenSymbol}
             </Text>
-            <Button variant={'outline'} isFullWidth>
+            <Button variant={'outline'} isFullWidth onClick={deposit} isLoading={status === PROCESSING}
+                    disabled={amount === '0'}
+                    loadingText={"Depositing"}>
               Deposit
             </Button>
           </Stack>
