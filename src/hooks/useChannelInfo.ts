48bb167bdc85b23a1fe8a643fe5be0592412567b
tunogya <tunogya@qq.com>
2021-12-20 18:28:47 +0800

    update

--- src/hooks/useChannelInfo.ts
+++ src/hooks/useChannelInfo.ts
@@ -2,7 +2,7 @@ import {useNestOpenPlatformContract} from "./useContract";
 import {NEST_OPEN_PLATFORM_ADDRESS} from "../constants/addresses";
 import {useActiveWeb3React} from "./web3";
 import {useEffect, useState} from "react";
-import {formatNumber, parseToBigNumber} from "../utils/bignumberUtil";
+import {parseToBigNumber} from "../utils/bignumberUtil";
 import {IDLE, IDLE_DELAY, PROCESSING} from "../constants/misc";
 import {useRecoilState} from "recoil";
 import {activeChannelInfoAtom} from "../state/Root";
@@ -37,26 +37,26 @@ export const useChannelInfo = (channelId: string) => {
       const info: ChannelInfo = {
         channelId: channelId,
         // fee balance
-        feeInfo: parseToBigNumber(res.feeInfo).shiftedBy(-18).toFixed(0),
+        feeInfo: parseToBigNumber(res.feeInfo).shiftedBy(-18).toString(),
         // quotation fee
-        postFeeUnit: parseToBigNumber(res.postFeeUnit).shiftedBy(-4).toFixed(0),
+        postFeeUnit: parseToBigNumber(res.postFeeUnit).shiftedBy(-4).toString(),
         // attenuation factor:
-        reductionRate: parseToBigNumber(res.reductionRate).shiftedBy(-2).toFixed(0),
+        reductionRate: parseToBigNumber(res.reductionRate).shiftedBy(-2).toString(),
         // mining token
         reward: res.reward,
         // standard output
-        rewardPerBlock: parseToBigNumber(res.rewardPerBlock).shiftedBy(-18).toFixed(0),
+        rewardPerBlock: parseToBigNumber(res.rewardPerBlock).shiftedBy(-18).toString(),
         // number of quote
-        sheetCount: parseToBigNumber(res.sheetCount).toFixed(0),
+        sheetCount: parseToBigNumber(res.sheetCount).toString(),
         // price calling fee
-        singleFee: parseToBigNumber(res.singleFee).shiftedBy(-4).toFixed(0),
+        singleFee: parseToBigNumber(res.singleFee).shiftedBy(-4).toString(),
         // price token
         token0: res.token0,
         // quotation token
         token1: res.token1,
-        unit: parseToBigNumber(res.unit).shiftedBy(-18).toFixed(0),
-        vault: parseToBigNumber(res.vault).shiftedBy(-18).toFixed(0),
-        genesisBlock: parseToBigNumber(res.genesisBlock).toFixed(0),
+        unit: parseToBigNumber(res.unit).shiftedBy(-18).toString(),
+        vault: parseToBigNumber(res.vault).shiftedBy(-18).toString(),
+        genesisBlock: parseToBigNumber(res.genesisBlock).toString(),
         governance: res.governance,
       }
       setInfo(info)
