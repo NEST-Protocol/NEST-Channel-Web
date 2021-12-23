35ba78abdd6645c49f84e8e8e34009ba668c12c7
tunogya <tunogya@qq.com>
2021-12-23 12:11:06 +0800

    delete unused code

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,17 +1,12 @@
 import { Contract } from '@ethersproject/contracts'
 
 import ERC20_ABI from '../abis/erc20.json'
-import ERC20_BYTES32_ABI from '../abis/erc20_bytes32.json'
-import EIP_2612 from '../abis/eip_2612.json'
 import NestOpenPlatform_ABI from '../abis/nestOpenPlatform.json'
-import NestBatchPlatform2_ABI from '../abis/nestBatchPlatform2.json'
-import NestLedger_ABI from '../abis/nestLedger.json'
-import NestGovernance_ABI from '../abis/nestGovernance.json'
 
 import { useActiveWeb3React } from './web3'
 import { getContract } from '../utils'
 import { useMemo } from 'react'
-import { Erc20, NestOpenPlatform, NestBatchPlatform2, NestGovernance, NestLedger } from '../abis/types'
+import { Erc20, NestOpenPlatform } from '../abis/types'
 
 // returns null on errors
 export function useContract<T extends Contract = Contract>(
@@ -40,26 +35,6 @@ export function useTokenContract(tokenAddress?: string, withSignerIfPossible?: b
   return useContract<Erc20>(tokenAddress, ERC20_ABI, withSignerIfPossible)
 }
 
-export function useBytes32TokenContract(tokenAddress?: string, withSignerIfPossible?: boolean): Contract | null {
-  return useContract(tokenAddress, ERC20_BYTES32_ABI, withSignerIfPossible)
-}
-
-export function useEIP2612Contract(tokenAddress?: string): Contract | null {
-  return useContract(tokenAddress, EIP_2612, false)
-}
-
 export function useNestOpenPlatformContract(address: string, withSignerIfPossible?: boolean): Contract | null {
   return useContract<NestOpenPlatform>(address, NestOpenPlatform_ABI, withSignerIfPossible)
-}
-
-export function useNestBatchPlatform2Contract(address: string, withSignerIfPossible?: boolean): Contract | null {
-  return useContract<NestBatchPlatform2>(address, NestBatchPlatform2_ABI, withSignerIfPossible)
-}
-
-export function useNestLedgerContract(address: string, withSignerIfPossible?: boolean): Contract | null {
-  return useContract<NestLedger>(address, NestLedger_ABI, withSignerIfPossible)
-}
-
-export function useNestGovernanceContract(address: string, withSignerIfPossible?: boolean): Contract | null {
-  return useContract<NestGovernance>(address, NestGovernance_ABI, withSignerIfPossible)
 }
