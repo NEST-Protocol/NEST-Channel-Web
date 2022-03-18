1058be0d9bccaa6aeb625e43130fc4090772714c
tunogya <tunogya@qq.com>
2022-03-18 14:45:57 +0800

    fixed

--- src/hooks/useChannelInfo.ts
+++ src/hooks/useChannelInfo.ts
@@ -1,12 +1,11 @@
 import { useNestOpenPlatformContract } from './useContract'
 import { useCallback, useEffect, useState } from 'react'
 import { IDLE, IDLE_DELAY, PROCESSING } from '../constants/misc'
-import { useRecoilState } from 'recoil'
-import { activeChannelInfoAtom } from '../state/Summary'
+import {defaultChannelInfo} from '../state/Summary'
 
 export const useChannelInfo = (channelId: string) => {
   const nestOpenPlatform = useNestOpenPlatformContract(false)
-  const [info, setInfo] = useRecoilState(activeChannelInfoAtom)
+  const [info, setInfo] = useState(defaultChannelInfo)
   const [status, setStatus] = useState(IDLE)
 
   const refresh = useCallback(async () => {
