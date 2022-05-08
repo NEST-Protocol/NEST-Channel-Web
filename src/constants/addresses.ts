ec7d1f2b0b4b76449e576eaa1c52a53c41eac23c
tunogya <tunogya@qq.com>
2022-05-08 16:37:27 +0800

    update

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -2,32 +2,13 @@ import { SupportedChainId } from './chains'
 
 type AddressMap = { [chainId: number]: string }
 
-export const NEST_ADDRESS: AddressMap = {
-  [SupportedChainId.MAINNET]: '0x04abEdA201850aC0124161F037Efd70c74ddC74C',
-  [SupportedChainId.RINKEBY]: '0xE313F3f49B647fBEDDC5F2389Edb5c93CBf4EE25',
-  [SupportedChainId.BSC]: '0x98f8669F6481EbB341B522fCD3663f79A3d1A6A7',
-  [SupportedChainId.BSCTestnet]: '0x821edD79cc386E56FeC9DA5793b87a3A52373cdE',
-  [SupportedChainId.KCC]: '0x98f8669F6481EbB341B522fCD3663f79A3d1A6A7',
-}
-
 export const PUSD_ADDRESS: AddressMap = {
-  // [SupportedChainId.MAINNET]: '',
+  [SupportedChainId.MAINNET]: '0xCCEcC702Ec67309Bc3DDAF6a42E9e5a6b8Da58f0',
   [SupportedChainId.RINKEBY]: '0x5407cab67ad304FB8A4aC46D83b3Dd63A9dbA575',
   [SupportedChainId.BSC]: '0x9b2689525e07406D8A6fB1C40a1b86D2cd34Cbb2',
   [SupportedChainId.BSCTestnet]: '0x3DA5c9aafc6e6D6839E62e2fB65825869019F291',
   [SupportedChainId.KCC]: '0x0C4CD7cA70172Af5f4BfCb7b0ACBf6EdFEaFab31',
-}
-
-export const USDT_ADDRESS: AddressMap = {
-  // [SupportedChainId.MAINNET]: '',
-  [SupportedChainId.RINKEBY]: '0x20125a7256EFafd0d4Eec24048E08C5045BC5900',
-  [SupportedChainId.BSC]: '0x55d398326f99059ff775485246999027b3197955',
-  [SupportedChainId.BSCTestnet]: '0xDd4A68D8236247BDC159F7C5fF92717AA634cBCc',
-}
-
-export const HBTC_ADDRESS: AddressMap = {
-  [SupportedChainId.MAINNET]: '0x0316EB71485b0Ab14103307bf65a021042c6d380',
-  [SupportedChainId.RINKEBY]: '0xaE73d363Cb4aC97734E07e48B01D0a1FF5D1190B',
+  [SupportedChainId.Polygon]: '0xf26D86043a3133Cc042221Ea178cAED7Fe0eE362',
 }
 
 export const NEST_OPEN_PLATFORM_ADDRESS: AddressMap = {
@@ -40,23 +21,11 @@ export const NEST_OPEN_PLATFORM_ADDRESS: AddressMap = {
 }
 
 export const PETH_ADDRESS: AddressMap = {
-  // [SupportedChainId.MAINNET]: '',
+  [SupportedChainId.MAINNET]: '0x0000000000000000000000000000000000000000',
   [SupportedChainId.RINKEBY]: '0xbe155CDf7F6dA37684A36DCC02076Ed314d5467a',
   [SupportedChainId.BSC]: '0x556d8bF8bF7EaAF2626da679Aa684Bac347d30bB',
   [SupportedChainId.BSCTestnet]: '0xc39dC1385a44fBB895991580EA55FC10e7451cB3',
   [SupportedChainId.KCC]: '0x6cce8b9da777Ab10B11f4EA8510447431ED6ad1E',
+  [SupportedChainId.Polygon]: '0x1E0967e10B5Ef10342d4D71da69c30332666C899'
 }
 
-export const USDC_ADDRESS: AddressMap = {
-  // [SupportedChainId.MAINNET]: '',
-  [SupportedChainId.RINKEBY]: '0xE313F3f49B647fBEDDC5F2389Edb5c93CBf4EE25',
-  [SupportedChainId.BSC]: '0xE313F3f49B647fBEDDC5F2389Edb5c93CBf4EE25',
-  [SupportedChainId.BSCTestnet]: '0xE313F3f49B647fBEDDC5F2389Edb5c93CBf4EE25',
-}
-
-export const COFIX_ADDRESS: AddressMap = {
-  // [SupportedChainId.MAINNET]: '',
-  [SupportedChainId.RINKEBY]: '0x61EA050b28Ccca539F0faf79Fd26F6Df31b9f15B',
-  [SupportedChainId.BSC]: '0x61EA050b28Ccca539F0faf79Fd26F6Df31b9f15B',
-  [SupportedChainId.BSCTestnet]: '0x61EA050b28Ccca539F0faf79Fd26F6Df31b9f15B',
-}
